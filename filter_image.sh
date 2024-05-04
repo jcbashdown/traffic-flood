6464af5e011103817a13c11989cc271cb4f51bb6
Jacob Ashdown <jcbashdown@gmail.com>
2024-05-04 23:35:53 +0300

    Convert to svg

--- filter_image.sh
+++ filter_image.sh
@@ -1,1 +1,3 @@
-convert screenshot.png -fuzz 30% -fill white +opaque "#F23C32" -fill white +opaque "#832525" output.png
+convert screenshot.png \
+\( +clone -fuzz 31% -fill black +opaque "#F23C32" -fill white -opaque "#832525" \) \
+-alpha off -compose copy_opacity -composite output.png
---
+++ simplify-to-svg.sh
@@ -0,0 +1,2 @@
+convert output.png output.pnm
+potrace output.pnm -s -o output.svg --alphamax 0.1 --turdsize 100
